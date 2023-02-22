d7cd93d779fb1e6813f50301388ddfd3edc0b7a2
Gideon <gt.riesebosch33@gmail.com>
2023-02-22 09:12:15 +0100

    inital commit

---
+++ TakenApp/ClassLibrary/Api.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Api
+    {
+        public List<Task> GetAllTasks()
+        {
+            return null;
+        }
+
+        private void UpdateTask(int id)
+        {
+
+        }
+
+        private void DeleteTask(int id)
+        {
+
+        }
+    }
+}
---
+++ TakenApp/ClassLibrary/Objects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Task
+    {
+        public int id { get; set; }
+        public string subject { get; set; }
+        public int priority { get; set; }
+        public string state { get; set; }
+        public DateTime date { get; set; }
+    }
+}
